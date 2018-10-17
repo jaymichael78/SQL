85bc7b5d40e6722fc0490ff4a15c27570cc6b42e
jaymichael78 <jaymichael78@users.noreply.github.com>
2018-10-17 12:49:27 -0500

    Update GetParamsAndConnections.sql

--- SSISDB/GetParamsAndConnections.sql
+++ SSISDB/GetParamsAndConnections.sql
@@ -1,6 +1,7 @@
 USE SSISDB;
 
-SELECT pr.name AS projectName, pr.project_id, pr.folder_id, 
+WITH projectGoodies AS (
+SELECT DISTINCT pr.name AS projectName, pr.project_id, pr.folder_id, 
 CASE 
 	WHEN par.object_type = 20 THEN 'Project'
 	WHEN par.object_type = 30 THEN 'Package'
@@ -19,4 +20,10 @@ par.referenced_variable_name AS environmentalVariableName
 FROM internal.projects pr
 JOIN internal.object_parameters par ON par.project_id = pr.project_id
 WHERE pr.project_id = 17 --OR change to pr.name = '' to get differernt projects
+)
+SELECT projectName, level, type, parameterName, hasEnvironmentalVariable, environmentalVariableName
+FROM projectGoodies
+WHERE level= 'Project' --Change to Package to get Package level
+AND type = 'Parameter' --Change to Connection Manager to get Connection information
+
 ;
